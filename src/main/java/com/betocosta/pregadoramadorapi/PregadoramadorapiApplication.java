7f222776b398b3c68ccd495af09ba33f5d04bdff
Alberto Costa <mihcadevs@gmail.com>
2020-09-13 11:06:27 -0300

    Criação do projeto

---
+++ src/main/java/com/betocosta/pregadoramadorapi/PregadoramadorapiApplication.java
@@ -0,0 +1,13 @@
+package com.betocosta.pregadoramadorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregadoramadorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PregadoramadorapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betocosta/pregadoramadorapi/PregadoramadorapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betocosta.pregadoramadorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PregadoramadorapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
